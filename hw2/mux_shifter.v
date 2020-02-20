cc2f29aa968e78eb52e5589c846f487d0dd8786f
Dragon201701 <liualbert1@gmail.com>
2020-02-19 18:41:27 -0600

    Complete HW2.2.

--- hw2/mux_shifter.v
+++
@@ -1,8 +0,0 @@
-module mux_shifter (prev, cur, next, op, en, out);
-	input prev, cur, next, en;
-	input [1:0] op;
-	output out;
-	wire opresult;
-	mux2_1 opmux(.InA(prev), .InB(next), .S(op[1]), .Out(opresult));
-	mux2_1 shift_en(.InA(cur), .InB(opresult), .S(en), .Out(out));
-endmodule
